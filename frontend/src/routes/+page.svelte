00dbfe0f6056a1367ee5e7d22d3241b40c499042
Rehaan12345 <rehaan1099@gmail.com>
2024-06-04 21:11:49 -0400

    initial setup complete

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { onMount } from "svelte";
+
+    let data;
+
+    async function start() {
+        try {
+            const url = "http://localhost:8000/";
+            const res = await fetch(url);
+            if (!res.ok) {
+                throw new Error("Fetching didn't work");
+            }
+            const resData = await res.json();
+            data = resData["data"];
+        } catch (error) {
+            console.log("Error retrieving data: ", error);
+        }
+    }
+
+    onMount(() => { start(); })
+</script>
+
+<h1>{data}</h1>
